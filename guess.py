4703ced1a62ec5e68536b1ebbbc3b963f52dfa10
Deepraj Baidya <deeprajbaidya@gmail.com>
2020-12-11 11:49:28 +0530

    second commit

--- guess.py
+++ guess.py
@@ -8,7 +8,7 @@ msg = input("Would You Like to Play the Game[y/n]:- ")
 
 while msg == 'y':
     while(user != randNumber):
-        user = int(input("Enter your guess: "))
+        user = int(input("Enter your guess between (1-100): "))
         chance += 1
         if(user == randNumber):
             print("You guessed it right! Congrats...")
