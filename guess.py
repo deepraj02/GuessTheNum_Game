fc226008d6b22d0f03fc37dd7c54331e0be399fb
Deepraj Baidya <deeprajbaidya@gmail.com>
2020-12-13 11:07:33 +0530

    Enabled Looping while playing the game.

--- guess.py
+++ guess.py
@@ -4,19 +4,21 @@ user = None
 chance = 0
 
 print("\t Welcome To Number Guessing Game \t")
-msg = input("Would You Like to Play the Game[y/n]:- ")
 
-while msg == 'y':
-    while(user != randNumber):
-        user = int(input("Enter your guess between (1-100): "))
-        chance += 1
-        if(user == randNumber):
-            print("You guessed it right! Congrats...")
-        else:
-            if(user > randNumber):
-                print("You guessed it wrong! Enter a smaller number")
+while True:
+    msg = input("Would You Like to Play the Game[y/n]:- ")
+    if msg=='y':
+        while(user != randNumber):
+            user = int(input("Enter your guess between (1-100): "))
+            chance += 1
+            if(user == randNumber):
+                print("\nYou WON,You Guessed it Right ")
+                print(f"\tYou guessed the number in {chance} guesses")
             else:
-                print("You guessed it wrong! Enter a larger number")
+                if(user > randNumber):
+                    print("You guessed it wrong! Enter a smaller number")
+                else:
+                    print("You guessed it wrong! Enter a larger number")
     else:
+        print("Hope You Enjoyed the Game.")
         break
-print(f"You guessed the number in {chance} guesses")
